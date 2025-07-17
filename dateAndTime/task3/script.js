734411ae99e21bbc31bb9c6f18d702a04a80ef11
cristianchilat <chilatcristan@gmail.com>
2025-07-17 16:20:34 +0300

    JS dateAndTime task3

---
+++ dateAndTime/task3/script.js
@@ -0,0 +1,16 @@
+Date.prototype.getMonthName = function () {
+	if (this.getMonth() === 0) {return 'January';}
+	if (this.getMonth() === 1) {return 'February';}
+	if (this.getMonth() === 2) {return 'March';}
+	if (this.getMonth() === 3) {return 'April';}
+	if (this.getMonth() === 4) {return 'May';}
+	if (this.getMonth() === 5) {return 'June';}
+	if (this.getMonth() === 6) {return 'July';}
+	if (this.getMonth() === 7) {return 'August';}
+	if (this.getMonth() === 8) {return 'September';}
+	if (this.getMonth() === 9) {return 'October';}
+	if (this.getMonth() === 10) {return 'November';}
+	if (this.getMonth() === 11) {return 'December';}
+}
+
+console.log(new Date().getMonthName());
