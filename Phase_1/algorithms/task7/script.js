4f656f9370ec5cde49409ce2c905befd5fd52452
cristianchilat <chilatcristan@gmail.com>
2025-08-13 13:37:56 +0300

    Divided everything in 2 phases and corected task 10

---
+++ Phase_1/algorithms/task7/script.js
@@ -0,0 +1,25 @@
+// let matrix = [];
+// let sizeOfMatrix = 6;
+// let currentNumber = 0;
+// let accumulatedNumber = 0;
+//
+// function initMatrix() {
+// 	for (let i = 0; i < sizeOfMatrix; i++) {
+// 		matrix[i] = [];
+// 		for (let j = 0; j < sizeOfMatrix; j++) {
+// 			matrix[i][j] = 0;
+//
+// 		}
+// 	}
+// }
+//
+// function fillMatrix() {
+// 	for (let i = 0; i < sizeOfMatrix; i++) {
+// 		for (let j = 0; j < sizeOfMatrix; j++) {
+//
+// 	}
+// }
+//
+// initMatrix();
+// fillMatrix();
+
--- Phase_1/fundamentals/task10/script.js
+++ Phase_1/fundamentals/task10/script.js
@@ -1,11 +1,12 @@
 const number = 15;
 
 const dividers = (number) => {
-	for (let i = number; i >= 0; i--) {
+	for (let i = 0; i <= number / 2; i++) {
 		if (number % i === 0) {
 			console.log(i);
 		}
 	}
+	console.log(number);
 }
 
 dividers(number);
