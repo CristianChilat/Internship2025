0206ca7ca9cdddcc03e72f780ec76f3713cbe422
cristianchilat <chilatcristan@gmail.com>
2025-08-15 14:53:24 +0300

    Task 2,3 finished

--- Phase_2/task1_2_3_4/script.js
+++ Phase_2/task1_2_3_4/script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 	document.body.style.backgroundColor = "red";
+
+	setTimeout(() => {
+		document.body.style.backgroundColor = "green";
+	}, 3000)
+})
+
+const buttonElement = document.getElementById("button-orange");
+
+buttonElement.addEventListener("click", () => {
+	document.body.style.backgroundColor = "orange";
 })
