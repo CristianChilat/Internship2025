c9aa2d5aa78cf5d3d6e0942dab94d4608e49feb6
cristianchilat <chilatcristan@gmail.com>
2025-08-21 18:02:19 +0300

    task 17 finished

--- Phase_2/task17/script.js
+++ Phase_2/task17/script.js
@@ -1,9 +1,7 @@
 const element = document.getElementById("elem");
 
 element.addEventListener("click", () => {
-	alert(`The width of this element is: ${element.offsetWidth}px and height is: ${element.offsetHeight}px`);
-	element.style.width = `${element.offsetWidth * 2}px`;
-	element.style.height = `${element.offsetHeight * 2}px`;
+	alert(`The width of this element is: ${element.scrollWidth}px and height is: ${element.scrollHeight}px`);
+	element.style.width = `${element.scrollWidth * 2}px`;
+	element.style.height = `${element.scrollHeight * 2}px`;
 })
-
-//todo find out how to ignore padding on click event
