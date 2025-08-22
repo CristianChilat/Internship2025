00b338a806341d3b9f22d2374bdd26950a65e581
cristianchilat <chilatcristan@gmail.com>
2025-08-22 15:29:38 +0300

    task 19 finished

--- Phase_2/task19/script.js
+++ Phase_2/task19/script.js
@@ -2,37 +2,31 @@ const childElement = document.getElementById("child");
 const leftParentElement = document.getElementById("left-parent");
 const rightParentElement = document.getElementById("right-parent");
 
-rightParentElement.onmouseover = function() {
-	rightParentElement.style.backgroundColor = 'red';
-};
+childElement.addEventListener('dragstart', (event) => {
+	event.dataTransfer.setData('text', event.target.id);
+	event.target.style.opacity = '0.5';
+})
 
-rightParentElement.onmouseout = function() {
-	rightParentElement.style.backgroundColor = 'blue';
-};
+childElement.addEventListener('dragend', (event) => {
+	event.target.style.opacity = '1';
+})
 
-childElement.onmousedown = function(mouseEvent) {
-	childElement.style.position = 'absolute';
-	childElement.style.zIndex = '100';
 
-	document.body.append(childElement);
+rightParentElement.addEventListener('dragover', (event) => {
+	event.preventDefault();
+})
 
-	function moveElementWithCursor(pageX, pageY) {
-		childElement.style.left = pageX - childElement.offsetWidth / 2 + 'px';
-		childElement.style.top = pageY - childElement.offsetHeight / 2 + 'px';
-	}
+rightParentElement.addEventListener('drop', (event) => {
+	event.preventDefault();
+	event.target.appendChild(childElement);
+})
 
-	moveElementWithCursor(mouseEvent.pageX, mouseEvent.pageY);
 
-	function onMouseMove(mouseEvent) {
-		moveElementWithCursor(mouseEvent.pageX, mouseEvent.pageY);
-	}
+leftParentElement.addEventListener('dragover', (event) => {
+	event.preventDefault();
+})
 
-	document.addEventListener('mousemove', onMouseMove);
-
-	childElement.onmouseup = function() {
-		document.removeEventListener('mousemove', onMouseMove);
-		childElement.onmouseup = null;
-	};
-};
-
-//todo to make so when element is over an parent it will assign to it
+leftParentElement.addEventListener('drop', (event) => {
+	event.preventDefault();
+	event.target.appendChild(childElement);
+})
